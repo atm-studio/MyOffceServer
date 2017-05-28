44a9ddd35f59115a9158ec037c513b445d55ab2e
Anatoly Makshantsev <a.makshantsev@gmail.com>
2017-05-28 12:55:47 +0400

    Rest service

--- src/main/java/com/tm_studio/myoffice/server/controller/OfficeController.java
+++ src/main/java/com/tm_studio/myoffice/server/controller/OfficeController.java
@@ -1,19 +1,29 @@
 package com.tm_studio.myoffice.server.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
+import com.tm_studio.myoffice.server.entity.Remind;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import java.util.Date;
+
+@RestController
 @RequestMapping("/office")
 public class OfficeController {
 
     @RequestMapping(value = "/get", method = RequestMethod.GET)
     @ResponseBody
-    public String getNotification(ModelMap model) {
-        return "MyOffice";
+    public Remind getNotification() {
+        return createMockRemind();
+    }
+
+    private Remind createMockRemind() {
+        Remind remind = new Remind();
+        remind.setId(1);
+        remind.setMeetDate(new Date());
+        remind.setTitle("My first Meet");
+        return  remind;
     }
 
 }
